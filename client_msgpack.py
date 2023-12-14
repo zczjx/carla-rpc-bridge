0b45ac211055ebd1bd481be58f64c2b162a17de1
northbigpanda <businiao1988@gmail.com>
2023-12-14 22:10:30 +0800

    delete other file, only keep manual control multicam.py

--- client_msgpack.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import msgpackrpc
-
-if __name__ == '__main__':
-    client = msgpackrpc.Client(msgpackrpc.Address("localhost", 18800))
-    result = client.call('get_rig')
-    print(type(result))
-    print(result)
-    client.call('push_data', [4, 9 ,2, 8, 7])
